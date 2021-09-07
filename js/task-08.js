ed39d7a73d31308458bfa9e642a0529bd2a00425
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-08 01:02:22 +0300

    Рефакторинг задачи 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,7 @@
-/* Задание 8 - дополнительное, выполнять не обязательно
-Напиши скрипт создания и очистки коллекции элементов. Пользователь вводит количество элементов в input и нажимает кнопку Создать, после чего рендерится коллекция. При нажатии на кнопку Очистить, коллекция элементов очищается.
-
+/* 
 Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число. Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
-
-Каждый созданный div:
-
-Имеет случайный rgb цвет фона
-Размеры самого первого div - 30px на 30px
-Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
 Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-<div id="controls">
-  <input type="number" min="0" max="100" step="1" />
-  <button type="button" data-action="render">Создать</button>
-  <button type="button" data-action="destroy">Очистить</button>
-</div>
-
-<div id="boxes"></div> */
+*/
 
 const refs = {
   inputEl: document.querySelector('input'),
@@ -25,41 +10,40 @@ const refs = {
   boxesDivEl: document.querySelector('#boxes'),
 };
 
-let arrayToCreate = [];
-let inputValue;
-let rgb;
-
-refs.renderBtn.addEventListener('click', onRenderBtnClick);
 refs.inputEl.addEventListener('blur', onInputChange);
+refs.renderBtn.addEventListener('click', onRenderBtnClick);
+refs.destroyBtn.addEventListener('click', onDestroyBtnClick);
+
+let markupArray = [];
 
 function onInputChange() {
-  inputValue = refs.inputEl.value;
-}
-
-function randomColor() {
-  let r = Math.round(Math.random() * 256); // тут мат сел и 257
-  let g = Math.round(Math.random() * 256);
-  let b = Math.round(Math.random() * 256);
-  return (rgb = `rgb(${r},${g},${b})`);
+  return refs.inputEl.value;
 }
 
 function onRenderBtnClick() {
-  arrayToCreate = [];
+  markupArray = [];
   refs.boxesDivEl.innerHTML = '';
-  for (let i = 1; i <= inputValue; i += 1) {
-    // тут подумать как заменить фор и возможно избавится от массива?
+
+  for (let i = 1; i <= onInputChange(); i += 1) {
     let size = 10 + 5 * i;
-    arrayToCreate.push(
-      `<div style="display: inline-block; background-color: ${randomColor()}; padding: ${size}px; margin: 5px"></div>`,
-    );
+    let markupString = `<div style="display: inline-block;
+                          background-color: ${randomColor()}; 
+                          padding: ${size}px; 
+                          margin: 5px">
+                        </div>`;
+    markupArray.push(markupString);
   }
-  let string = arrayToCreate.join('');
-  return refs.boxesDivEl.insertAdjacentHTML('afterbegin', string);
+  return refs.boxesDivEl.insertAdjacentHTML('afterbegin', markupArray.join(''));
 }
 
-refs.destroyBtn.addEventListener('click', onDestroyBtnClick);
-
 function onDestroyBtnClick() {
-  arrayToCreate = [];
+  refs.inputEl.value = 0;
   return (refs.boxesDivEl.innerHTML = '');
+}
+
+function randomColor() {
+  let r = Math.ceil(Math.random() * 257);
+  let g = Math.ceil(Math.random() * 257);
+  let b = Math.ceil(Math.random() * 257);
+  return `rgb(${r},${g},${b})`;
 }
